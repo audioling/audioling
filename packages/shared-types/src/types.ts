696b1513d8932101605c7a6f7a3f33966209d727
jeffvli <jeffvictorli@gmail.com>
2024-10-02 12:17:50 -0700

    refactor(server): add shared list sort options

--- packages/shared-types/src/types.ts
+++ packages/shared-types/src/types.ts
@@ -4,6 +4,17 @@ export enum LibraryType {
     SUBSONIC = 'SUBSONIC',
 }
 
+export enum LibraryItemType {
+    ALBUM = 'album',
+    ALBUM_ARTIST = 'albumArtist',
+    ARTIST = 'artist',
+    GENRE = 'genre',
+    LIBRARY = 'library',
+    PLAYLIST = 'playlist',
+    TRACK = 'track',
+    USER = 'user',
+}
+
 export type LibraryFeatures = {
     album: {
         detail: {
@@ -36,10 +47,17 @@ export type LibraryFeatures = {
         list: {
             enabled: boolean;
             filter: {
+                album: boolean;
+                albumCount: boolean;
+                dateAdded: boolean;
+                duration: boolean;
+                favorited: boolean;
                 name: boolean;
                 playCount: boolean;
+                random: boolean;
+                rating: boolean;
                 releaseDate: boolean;
-                year: boolean;
+                songCount: boolean;
             };
         };
     };
@@ -53,12 +71,16 @@ export type LibraryFeatures = {
         list: {
             enabled: boolean;
             filter: {
+                duration: boolean;
                 name: boolean;
+                owner: boolean;
+                public: boolean;
                 songCount: boolean;
+                updatedAt: boolean;
             };
         };
     };
-    song: {
+    track: {
         detail: {
             enabled: boolean;
             topSongList: boolean;
@@ -84,3 +106,87 @@ export type LibraryFeatures = {
         };
     };
 };
+
+// Sync with LibraryFeatures[album][list][filter]
+export enum AlbumListSortOptions {
+    ALBUM_ARTIST = 'albumArtist',
+    ARTIST = 'artist',
+    COMMUNITY_RATING = 'communityRating',
+    CRITIC_RATING = 'criticRating',
+    DATE_ADDED = 'dateAdded',
+    DATE_PLAYED = 'datePlayed',
+    DURATION = 'duration',
+    IS_FAVORITE = 'isFavorite',
+    NAME = 'name',
+    PLAY_COUNT = 'playCount',
+    RANDOM = 'random',
+    RELEASE_DATE = 'releaseDate',
+    SONG_COUNT = 'songCount',
+    YEAR = 'year',
+}
+
+// Sync with LibraryFeatures[albumArtist][list][filter]
+export enum ArtistListSortOptions {
+    ALBUM = 'album',
+    ALBUM_COUNT = 'albumCount',
+    DATE_ADDED = 'dateAdded',
+    DURATION = 'duration',
+    FAVORITED = 'favorited',
+    NAME = 'name',
+    PLAY_COUNT = 'playCount',
+    RANDOM = 'random',
+    RATING = 'rating',
+    RELEASE_DATE = 'releaseDate',
+    SONG_COUNT = 'songCount',
+}
+
+// Sync with LibraryFeatures[playlist][list][filter]
+export enum PlaylistListSortOptions {
+    DURATION = 'duration',
+    NAME = 'name',
+    OWNER = 'owner',
+    PUBLIC = 'public',
+    SONG_COUNT = 'songCount',
+    UPDATED_AT = 'updatedAt',
+}
+
+// Sync with LibraryFeatures[track][list][filter]
+export enum TrackListSortOptions {
+    ALBUM = 'album',
+    ALBUM_ARTIST = 'albumArtist',
+    ARTIST = 'artist',
+    BPM = 'bpm',
+    CHANNELS = 'channels',
+    COMMENT = 'comment',
+    DURATION = 'duration',
+    FAVORITED = 'favorited',
+    GENRE = 'genre',
+    ID = 'id',
+    NAME = 'name',
+    PLAY_COUNT = 'playCount',
+    RANDOM = 'random',
+    RATING = 'rating',
+    RECENTLY_ADDED = 'recentlyAdded',
+    RECENTLY_PLAYED = 'recentlyPlayed',
+    RELEASE_DATE = 'releaseDate',
+    YEAR = 'year',
+}
+
+export enum LibraryListSortOptions {
+    CREATED_AT = 'createdAt',
+    NAME = 'name',
+    TYPE = 'type',
+    UPDATED_AT = 'updatedAt',
+}
+
+export enum UserListSortOptions {
+    CREATED_AT = 'createdAt',
+    DISPLAY_NAME = 'displayName',
+    NAME = 'name',
+    UPDATED_AT = 'updatedAt',
+}
+
+export enum ListSortOrder {
+    ASC = 'asc',
+    DESC = 'desc',
+}
