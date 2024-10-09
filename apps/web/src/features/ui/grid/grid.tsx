31e3e65e111b0c692de2e9a433dd783edc847b5c
jeffvli <jeffvictorli@gmail.com>
2024-10-08 18:37:55 -0700

    refactor(web): add gap/grow to grid component

--- apps/web/src/features/ui/grid/grid.tsx
+++ apps/web/src/features/ui/grid/grid.tsx
@@ -1,20 +1,24 @@
 import { Grid as MantineGrid } from '@mantine/core';
+import clsx from 'clsx';
+import styles from './grid.module.scss';
 
 interface GridProps extends React.ComponentPropsWithoutRef<'div'> {
     align?: 'start' | 'end' | 'center' | 'baseline' | 'stretch';
     children: React.ReactNode;
     columns?: number;
+    gap?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
     grow?: boolean;
     justify?: 'start' | 'end' | 'center' | 'between' | 'around';
 }
 
 export const Grid = (props: GridProps) => {
-    const { align, children, columns, grow, justify, ...htmlProps } = props;
+    const { align, children, columns, gap, grow, justify, ...htmlProps } = props;
     return (
         <MantineGrid
             align={getAlign(align)}
             columns={columns}
             grow={grow}
+            gutter={gap}
             justify={getJustify(justify)}
             {...htmlProps}
         >
@@ -25,15 +29,24 @@ export const Grid = (props: GridProps) => {
 
 interface GridColProps extends React.ComponentPropsWithoutRef<'div'> {
     children: React.ReactNode;
+    grow?: boolean;
     offset?: number;
     order?: number;
     span?: number;
 }
 
 export const GridCol = (props: GridColProps) => {
-    const { children, offset, order, span, ...htmlProps } = props;
+    const { children, offset, order, span, grow, ...htmlProps } = props;
+
+    const classNames = {
+        col: clsx(styles.col, {
+            [styles.grow]: grow,
+        }),
+    };
+
     return (
         <MantineGrid.Col
+            classNames={classNames}
             offset={offset}
             order={order}
             span={span}
@@ -44,11 +57,9 @@ export const GridCol = (props: GridColProps) => {
     );
 };
 
-// export const MotionGrid = motion(Grid);
-
 Grid.displayName = 'Grid';
 
-Grid.Col = MantineGrid.Col;
+Grid.Col = GridCol;
 
 function getJustify(justify: GridProps['justify']) {
     switch (justify) {
