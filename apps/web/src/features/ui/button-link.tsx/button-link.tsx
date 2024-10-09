21620cc20f3619fef6dbe6264eec016fe3215f00
jeffvli <jeffvictorli@gmail.com>
2024-10-08 22:06:09 -0700

    refactor(web): normalize ButtonLink props with Button component

--- apps/web/src/features/ui/button-link.tsx/button-link.tsx
+++ apps/web/src/features/ui/button-link.tsx/button-link.tsx
@@ -4,15 +4,54 @@ import type { ButtonProps } from '@/features/ui/button/button.tsx';
 import { Button } from '@/features/ui/button/button.tsx';
 
 interface ButtonLinkProps extends LinkProps {
-    buttonProps?: ButtonProps;
+    children?: React.ReactNode;
+    isDisabled?: ButtonProps['isDisabled'];
+    isLoading?: ButtonProps['isLoading'];
+    justify?: ButtonProps['justify'];
+    leftIcon?: ButtonProps['leftIcon'];
+    leftIconProps?: ButtonProps['leftIconProps'];
+    radius?: ButtonProps['radius'];
+    rightIcon?: ButtonProps['rightIcon'];
+    rightIconProps?: ButtonProps['rightIconProps'];
+    size?: ButtonProps['size'];
+    uppercase?: ButtonProps['uppercase'];
+    variant?: ButtonProps['variant'];
 }
 
 export const ButtonLink = (props: ButtonLinkProps) => {
-    const { buttonProps, children, ...linkProps } = props;
+    const {
+        children,
+        isDisabled,
+        isLoading,
+        justify,
+        leftIcon,
+        leftIconProps,
+        radius,
+        rightIcon,
+        rightIconProps,
+        size,
+        uppercase,
+        variant,
+        ...linkProps
+    } = props;
 
     return (
         <Link {...linkProps}>
-            <Button {...buttonProps}>{children}</Button>
+            <Button
+                isDisabled={isDisabled}
+                isLoading={isLoading}
+                justify={justify}
+                leftIcon={leftIcon}
+                leftIconProps={leftIconProps}
+                radius={radius}
+                rightIcon={rightIcon}
+                rightIconProps={rightIconProps}
+                size={size}
+                uppercase={uppercase}
+                variant={variant}
+            >
+                {children}
+            </Button>
         </Link>
     );
 };
