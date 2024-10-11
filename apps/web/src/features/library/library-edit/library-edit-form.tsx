20ce6e1724279d02bfbea002c6f7c7876f8fffd7
jeffvli <jeffvictorli@gmail.com>
2024-10-10 20:09:15 -0700

    refactor(web): add edit library form

--- apps/web/src/features/library/library-edit/library-edit-form.tsx
+++ apps/web/src/features/library/library-edit/library-edit-form.tsx
@@ -1,3 +1,185 @@
+import type { FormEvent } from 'react';
+import { LibraryListSortOptions, LibraryType, ListSortOrder } from '@repo/shared-types';
+import { useForm } from '@tanstack/react-form';
+import { useQueryClient } from '@tanstack/react-query';
+import { generatePath, useNavigate, useParams } from 'react-router-dom';
+import type { PutApiLibrariesIdBody } from '@/api/openapi-generated/audioling-openapi-client.schemas.ts';
+import {
+    getGetApiLibrariesQueryKey,
+    useDeleteApiLibrariesId,
+    useGetApiLibrariesIdSuspense,
+    usePutApiLibrariesId,
+} from '@/api/openapi-generated/libraries/libraries.ts';
+import JellyfinIcon from '@/assets/logos/jellyfin.png';
+import NavidromeIcon from '@/assets/logos/navidrome.png';
+import SubsonicIcon from '@/assets/logos/opensubsonic.png';
+import { Button } from '@/features/ui/button/button.tsx';
+import { Divider } from '@/features/ui/divider/divider.tsx';
+import { PasswordInput } from '@/features/ui/password-input/password-input.tsx';
+import { Stack } from '@/features/ui/stack/stack.tsx';
+import { TextInput } from '@/features/ui/text-input/text-input.tsx';
+import { useFocusTrap } from '@/hooks/use-focus-trap.ts';
+import { APP_ROUTE } from '@/routes/app-routes.ts';
+
 export const LibraryEditForm = () => {
-    return <div>EditLibraryForm</div>;
+    const { libraryId } = useParams<{ libraryId: string }>() as { libraryId: string };
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
+    const { data: library } = useGetApiLibrariesIdSuspense(libraryId, {
+        query: {
+            gcTime: 0,
+            staleTime: 0,
+        },
+    });
+
+    const { mutate: editLibrary } = usePutApiLibrariesId();
+    const { mutate: removeLibrary, isPending: isRemovingLibrary } = useDeleteApiLibrariesId();
+
+    const { Field, handleSubmit, Subscribe } = useForm<PutApiLibrariesIdBody>({
+        defaultValues: {
+            baseUrl: library.data.baseUrl,
+            displayName: library.data.displayName,
+            password: '',
+            username: '',
+        },
+        onSubmit: async (e) => {
+            editLibrary(
+                {
+                    data: e.value,
+                    id: libraryId,
+                },
+                {
+                    onError: (error) => {
+                        // TODO: handle error
+                        console.error(error);
+                    },
+                    onSuccess: async () => {
+                        navigate(generatePath(APP_ROUTE.DASHBOARD_LIBRARY_SELECT));
+                        queryClient.invalidateQueries({
+                            queryKey: getGetApiLibrariesQueryKey({
+                                sortBy: LibraryListSortOptions.NAME,
+                                sortOrder: ListSortOrder.ASC,
+                            }),
+                        });
+                    },
+                },
+            );
+        },
+        validators: {
+            onChange: ({ value }) => {
+                return {
+                    fields: {
+                        baseUrl: value.baseUrl ? undefined : 'Base URL is required',
+                        displayName: value.displayName ? undefined : 'Display Name is required',
+                        password: value.password ? undefined : 'Password is required',
+                        username: value.username ? undefined : 'Username is required',
+                    },
+                };
+            },
+        },
+    });
+
+    const handleFormSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        handleSubmit();
+    };
+
+    const handleRemoveLibrary = () => {
+        removeLibrary(
+            { id: libraryId },
+            { onSuccess: () => navigate(generatePath(APP_ROUTE.DASHBOARD_LIBRARY_SELECT)) },
+        );
+    };
+
+    const ref = useFocusTrap(true);
+
+    return (
+        <Stack
+            ref={ref}
+            as="form"
+            onSubmit={handleFormSubmit}
+        >
+            <Field
+                children={(field) => (
+                    <TextInput
+                        data-autofocus
+                        label="Display Name"
+                        placeholder="My Library"
+                        rightSection={
+                            <img
+                                height="20px"
+                                src={
+                                    library.data.type === LibraryType.SUBSONIC
+                                        ? SubsonicIcon
+                                        : library.data.type === LibraryType.NAVIDROME
+                                          ? NavidromeIcon
+                                          : JellyfinIcon
+                                }
+                                width="20px"
+                            />
+                        }
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.currentTarget.value)}
+                    />
+                )}
+                name="displayName"
+            />
+            <Field
+                children={(field) => (
+                    <TextInput
+                        label="Base URL"
+                        placeholder="http://192.168.1.1:4533"
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.currentTarget.value)}
+                    />
+                )}
+                name="baseUrl"
+            />
+            <Field
+                children={(field) => (
+                    <TextInput
+                        autoComplete="username"
+                        label="Username"
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.currentTarget.value)}
+                    />
+                )}
+                name="username"
+            />
+            <Field
+                children={(field) => (
+                    <PasswordInput
+                        autoComplete="current-password"
+                        label="Password"
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.currentTarget.value)}
+                    />
+                )}
+                name="password"
+            />
+            <Subscribe
+                children={(props) => (
+                    <Button
+                        isDisabled={!props.canSubmit || props.isSubmitting}
+                        type="submit"
+                        variant="filled"
+                    >
+                        Save
+                    </Button>
+                )}
+            />
+            <Divider />
+            <Button
+                disabled={isRemovingLibrary}
+                variant="danger"
+                onClick={handleRemoveLibrary}
+            >
+                Remove library
+            </Button>
+        </Stack>
+    );
 };
--- apps/web/src/features/library/library-edit/library-edit-route.tsx
+++ apps/web/src/features/library/library-edit/library-edit-route.tsx
@@ -1,3 +1,34 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { useGetApiLibrariesIdSuspense } from '@/api/openapi-generated/libraries/libraries.ts';
+import { LibraryEditForm } from '@/features/library/library-edit/library-edit-form.tsx';
+import { Center } from '@/features/ui/center/center.tsx';
+import { Group } from '@/features/ui/group/group.tsx';
+import { IconButton } from '@/features/ui/icon-button/icon-button.tsx';
+import { Stack } from '@/features/ui/stack/stack.tsx';
+import { Title } from '@/features/ui/title/title.tsx';
+
 export const LibraryEditRoute = () => {
-    return <div>Edit Library</div>;
+    const navigate = useNavigate();
+    const { libraryId } = useParams<{ libraryId: string }>() as { libraryId: string };
+    const { data: library } = useGetApiLibrariesIdSuspense(libraryId);
+
+    return (
+        <Center>
+            <Stack w="320px">
+                <Group gap="xs">
+                    <IconButton
+                        icon="arrowLeft"
+                        onClick={() => navigate(-1)}
+                    />
+                    <Title
+                        order={1}
+                        size="lg"
+                    >
+                        Edit {library.data.displayName}
+                    </Title>
+                </Group>
+                <LibraryEditForm />
+            </Stack>
+        </Center>
+    );
 };
