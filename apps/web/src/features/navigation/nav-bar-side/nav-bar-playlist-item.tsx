c50525bcc5fb9fd1832ef9603458fa5f35045be0
jeffvli <jeffvictorli@gmail.com>
2024-10-24 20:03:04 -0700

    refactor(web): add drag/drop for card and sidebar items

--- apps/web/src/features/navigation/nav-bar-side/nav-bar-playlist-item.tsx
+++ apps/web/src/features/navigation/nav-bar-side/nav-bar-playlist-item.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useRef, useState } from 'react';
+import { dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+import clsx from 'clsx';
 import { generatePath, NavLink } from 'react-router-dom';
 import { Text } from '@/features/ui/text/text.tsx';
 import { APP_ROUTE } from '@/routes/app-routes.ts';
@@ -11,10 +14,29 @@ interface NavBarPlaylistItemProps {
 
 export function NavBarPlaylistItem(props: NavBarPlaylistItemProps) {
     const { libraryId, name, playlistId } = props;
+    const ref = useRef<HTMLAnchorElement>(null);
+    const [isDraggedOver, setIsDraggedOver] = useState(false);
+
+    useEffect(() => {
+        if (!ref.current) return;
+
+        return dropTargetForElements({
+            canDrop: () => {
+                return true;
+            },
+            element: ref.current,
+            onDragEnter: () => setIsDraggedOver(true),
+            onDragLeave: () => setIsDraggedOver(false),
+            onDrop: () => {
+                setIsDraggedOver(false);
+            },
+        });
+    }, []);
 
     return (
         <NavLink
-            className={styles.container}
+            ref={ref}
+            className={clsx(styles.container, { [styles.draggedOver]: isDraggedOver })}
             to={generatePath(APP_ROUTE.DASHBOARD_PLAYLISTS_DETAIL, {
                 libraryId: libraryId,
                 playlistId: playlistId,
--- apps/web/src/features/ui/card/album-card.tsx
+++ apps/web/src/features/ui/card/album-card.tsx
@@ -1,5 +1,13 @@
+import { useEffect, useRef, useState } from 'react';
+import { combine } from '@atlaskit/pragmatic-drag-and-drop/combine';
+import { draggable } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+import { disableNativeDragPreview } from '@atlaskit/pragmatic-drag-and-drop/element/disable-native-drag-preview';
+import { setCustomNativeDragPreview } from '@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview';
+import { LibraryItemType } from '@repo/shared-types';
 import clsx from 'clsx';
+import { createRoot } from 'react-dom/client';
 import { NavLink } from 'react-router-dom';
+import { DragPreview } from '@/features/ui/drag-preview/drag-preview.tsx';
 import styles from './album-card.module.scss';
 
 interface AlbumCardProps {
@@ -15,8 +23,39 @@ interface AlbumCardProps {
 }
 
 export function AlbumCard({ image, metadata, title }: AlbumCardProps) {
+    const ref = useRef<HTMLDivElement>(null);
+
+    const [isDragging, setIsDragging] = useState(false);
+
+    useEffect(() => {
+        if (!ref.current) return;
+
+        return combine(
+            draggable({
+                element: ref.current,
+                getInitialData: () => ({
+                    image,
+                    title: title.text,
+                    type: 'album',
+                }),
+                onDragStart: () => setIsDragging(true),
+                onDrop: () => setIsDragging(false),
+                onGenerateDragPreview: (data) => {
+                    disableNativeDragPreview({ nativeSetDragImage: data.nativeSetDragImage });
+                    setCustomNativeDragPreview({
+                        nativeSetDragImage: data.nativeSetDragImage,
+                        render: ({ container }) => {
+                            const root = createRoot(container);
+                            root.render(<DragPreview itemCount={1} type={LibraryItemType.ALBUM} />);
+                        },
+                    });
+                },
+            }),
+        );
+    }, [title.text, image]);
+
     return (
-        <div className={styles.card}>
+        <div ref={ref} className={clsx(styles.card, { [styles.dragging]: isDragging })}>
             <div className={styles.imageContainer}>
                 <img className={styles.image} src={image} />
             </div>
