451d55c1016024f0dbb42a8db6a2f7f527f7ff64
jeffvli <jeffvictorli@gmail.com>
2024-10-31 20:11:10 -0700

    refactor(web): add additional utility functions

--- apps/web/src/utils/drag-drop.ts
+++ apps/web/src/utils/drag-drop.ts
@@ -1,3 +1,4 @@
+import type { Edge } from '@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge';
 import { LibraryItemType } from '@repo/shared-types';
 
 export enum DragTarget {
@@ -35,5 +36,27 @@ export const dndUtils = {
     },
     isDropTarget: (target: DragTarget, types: DragTarget[]) => {
         return types.includes(target);
+    },
+    reorderById: (args: { edge: Edge | null; idFrom: string; idTo: string; list: string[] }) => {
+        const { edge, idFrom, idTo, list } = args;
+        const indexFrom = list.indexOf(idFrom);
+        const indexTo = list.indexOf(idTo);
+
+        // If dragging to the same position, do nothing
+        if (indexFrom === indexTo) return list;
+
+        // If dragging to the right, but is left edge, do nothing
+        if (edge === 'left' && indexTo > indexFrom) return list;
+
+        // If dragging to the left, but is right edge, do nothing
+        if (edge === 'right' && indexTo < indexFrom) return list;
+
+        return dndUtils.reorderByIndex({ index: indexFrom, list, newIndex: indexTo });
+    },
+    reorderByIndex: (args: { index: number; list: string[]; newIndex: number }) => {
+        const { list, index, newIndex } = args;
+        const newList = [...list];
+        newList.splice(newIndex, 0, newList.splice(index, 1)[0]);
+        return newList;
     },
 };
---
+++ apps/web/src/utils/throttle.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function throttle<T extends (...args: any[]) => unknown>(func: T, timeFrame: number) {
+    let lastTime = 0;
+    return function (...args: Parameters<T>) {
+        const now = new Date().valueOf();
+        if (now - lastTime >= timeFrame) {
+            func(...args);
+            lastTime = now;
+        }
+    };
+}
