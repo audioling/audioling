bec4e44c01ae215ac813d6bd1e85e616faf97435
jeffvli <jeffvictorli@gmail.com>
2024-11-03 12:19:23 -0800

    refactor(web): adjust animated page transition, add element props

--- apps/web/src/features/shared/components/animated-page.tsx
+++ apps/web/src/features/shared/components/animated-page.tsx
@@ -1,23 +1,30 @@
-import type { ReactNode } from 'react';
+import type { HTMLAttributes, ReactNode } from 'react';
 import clsx from 'clsx';
 import { motion } from 'framer-motion';
 import { animationVariants } from '@/features/ui/animations/variants.ts';
 import styles from './animated-page.module.scss';
 
-interface AnimatedPageProps {
+interface AnimatedPageProps
+    extends Omit<
+        HTMLAttributes<HTMLDivElement>,
+        'onAnimationStart' | 'onDragStart' | 'onDragEnd' | 'onAnimationEnd' | 'onDrag'
+    > {
     children: ReactNode;
     className?: string;
 }
 
-export function AnimatedPage({ children, className }: AnimatedPageProps) {
+export function AnimatedPage(props: AnimatedPageProps) {
+    const { children, className, ...htmlProps } = props;
+
     return (
         <motion.div
             animate="show"
             className={clsx(styles.container, className)}
             exit="hidden"
             initial="hidden"
-            transition={{ duration: 1, ease: 'easeInOut' }}
+            transition={{ duration: 0.5, ease: 'easeIn' }}
             variants={animationVariants.fadeIn}
+            {...htmlProps}
         >
             {children}
         </motion.div>
