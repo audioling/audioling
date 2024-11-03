8dc2c77e8c11cc77d63a6a94c9076be325ba57dc
jeffvli <jeffvictorli@gmail.com>
2024-11-03 12:19:33 -0800

    refactor(web): add animated page to home route

--- apps/web/src/features/dashboard/home/home-route.tsx
+++ apps/web/src/features/dashboard/home/home-route.tsx
@@ -1,14 +1,17 @@
 import { ItemCountDisplay } from '@/features/dashboard/home/item-count-display.tsx';
 import { MostPlayedAlbumCarousel } from '@/features/dashboard/home/most-played-album-carousel.tsx';
 import { RecentlyPlayedAlbumCarousel } from '@/features/dashboard/home/recently-played-album-carousel.tsx';
+import { AnimatedPage } from '@/features/shared/components/animated-page.tsx';
 import { Stack } from '@/features/ui/stack/stack.tsx';
 
 export function HomeRoute() {
     return (
-        <Stack id="home-route">
-            <ItemCountDisplay />
-            <RecentlyPlayedAlbumCarousel />
-            <MostPlayedAlbumCarousel />
-        </Stack>
+        <AnimatedPage id="home-route">
+            <Stack>
+                <ItemCountDisplay />
+                <RecentlyPlayedAlbumCarousel />
+                <MostPlayedAlbumCarousel />
+            </Stack>
+        </AnimatedPage>
     );
 }
