8134dc5ffaa5e839955cdcc975b46e7de9609606
jeffvli <jeffvictorli@gmail.com>
2024-11-04 21:53:21 -0800

    refactor(web): add suspense wrapper to playlist list

--- apps/web/src/features/navigation/nav-bar-side/nav-bar-playlist-list.tsx
+++ apps/web/src/features/navigation/nav-bar-side/nav-bar-playlist-list.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { ListSortOrder, PlaylistListSortOptions } from '@repo/shared-types';
 import { useParams } from 'react-router-dom';
 import {
@@ -6,11 +7,26 @@ import {
 } from '@/api/openapi-generated/playlists/playlists.ts';
 import { CreatePlaylistButton } from '@/features/navigation/nav-bar-side/create-playlist-button.tsx';
 import { NavBarPlaylistTree } from '@/features/navigation/nav-bar-side/nav-bar-playlist-tree.tsx';
+import { EmptyPlaceholder } from '@/features/ui/placeholders/empty-placeholder.tsx';
 import { ScrollArea } from '@/features/ui/scroll-area/scroll-area.tsx';
 import { Stack } from '@/features/ui/stack/stack.tsx';
 
 export function NavBarPlaylistList() {
+    return (
+        <ScrollArea>
+            <Stack gap="xs">
+                <CreatePlaylistButton />
+                <Suspense fallback={<EmptyPlaceholder />}>
+                    <PlaylistList />
+                </Suspense>
+            </Stack>
+        </ScrollArea>
+    );
+}
+
+function PlaylistList() {
     const { libraryId } = useParams() as { libraryId: string };
+
     const { data: playlists } = useGetApiLibraryIdPlaylistsSuspense(libraryId, {
         sortBy: PlaylistListSortOptions.NAME,
         sortOrder: ListSortOrder.ASC,
@@ -22,15 +38,10 @@ export function NavBarPlaylistList() {
     });
 
     return (
-        <ScrollArea>
-            <Stack gap="xs">
-                <CreatePlaylistButton />
-                <NavBarPlaylistTree
-                    folders={folders.data}
-                    libraryId={libraryId}
-                    playlists={playlists.data}
-                />
-            </Stack>
-        </ScrollArea>
+        <NavBarPlaylistTree
+            folders={folders.data}
+            libraryId={libraryId}
+            playlists={playlists.data}
+        />
     );
 }
