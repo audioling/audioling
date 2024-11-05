2018f29d6df6a5311f4bc6bd10245ba866dcefac
jeffvli <jeffvictorli@gmail.com>
2024-11-05 11:10:29 -0800

    refactor(web): support openDelay for tooltip

--- apps/web/src/features/ui/tooltip/tooltip.tsx
+++ apps/web/src/features/ui/tooltip/tooltip.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, useState } from 'react';
+import { type ReactNode, useEffect, useRef, useState } from 'react';
 import { Tooltip as MantineTooltip } from '@mantine/core';
 import type { FloatingPosition, TooltipProps as MantineTooltipProps } from '@mantine/core';
 import styles from './tooltip.module.scss';
@@ -6,15 +6,45 @@ import styles from './tooltip.module.scss';
 export interface TooltipProps {
     children: ReactNode;
     label: string;
+    openDelay?: number;
     position?: FloatingPosition;
 }
 
 export function Tooltip(props: TooltipProps) {
-    const { children, label, position = 'top' } = props;
+    const { children, label, openDelay = 0, position = 'top' } = props;
     const [opened, setOpened] = useState(false);
+    const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
 
     const tooltipClassNames: MantineTooltipProps['classNames'] = {
         tooltip: styles.tooltip,
+    };
+
+    useEffect(() => {
+        return () => {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+        };
+    }, []);
+
+    const handleMouseEnter = () => {
+        timeoutRef.current = setTimeout(() => {
+            setOpened(true);
+        }, openDelay);
+    };
+
+    const handleMouseLeave = () => {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+        setOpened(false);
+    };
+
+    const handleClick = () => {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+        setOpened(false);
     };
 
     return (
@@ -23,15 +53,16 @@ export function Tooltip(props: TooltipProps) {
             floatingStrategy="fixed"
             keepMounted={false}
             label={label}
+            openDelay={openDelay}
             opened={opened}
             position={position}
             transitionProps={{ duration: 300, transition: 'pop' }}
         >
             <div
                 className="al-tooltip-target"
-                onClick={() => setOpened(false)}
-                onMouseEnter={() => setOpened(true)}
-                onMouseLeave={() => setOpened(false)}
+                onClick={handleClick}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
             >
                 {children}
             </div>
