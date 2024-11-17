8443c4675508cc009dee7290a6dd0b2ae7c818c7
jeffvli <jeffvictorli@gmail.com>
2024-11-17 02:47:20 -0800

    refactor(web): move content padding to page container

--- apps/web/src/features/dashboard/home/home-route.tsx
+++ apps/web/src/features/dashboard/home/home-route.tsx
@@ -4,19 +4,25 @@ import { NewlyAddedAlbumCarousel } from '@/features/dashboard/home/newly-added-a
 import { RecentlyPlayedAlbumCarousel } from '@/features/dashboard/home/recently-played-album-carousel.tsx';
 import { ItemCountDisplay } from '@/features/dashboard/item-count/item-count-display.tsx';
 import { AnimatedContainer } from '@/features/shared/animated-container/animated-container.tsx';
+import { PageContainer } from '@/features/shared/page-container/page-container.tsx';
+import { ScrollArea } from '@/features/ui/scroll-area/scroll-area.tsx';
 import { Stack } from '@/features/ui/stack/stack.tsx';
 
 export function HomeRoute() {
     const { libraryId } = useParams() as { libraryId: string };
 
     return (
-        <AnimatedContainer id="home-route">
-            <Stack>
-                <ItemCountDisplay libraryId={libraryId} />
-                <RecentlyPlayedAlbumCarousel />
-                <NewlyAddedAlbumCarousel />
-                <MostPlayedAlbumCarousel />
-            </Stack>
-        </AnimatedContainer>
+        <ScrollArea>
+            <PageContainer id="home-route">
+                <AnimatedContainer>
+                    <Stack h="100%">
+                        <ItemCountDisplay libraryId={libraryId} />
+                        <RecentlyPlayedAlbumCarousel />
+                        <NewlyAddedAlbumCarousel />
+                        <MostPlayedAlbumCarousel />
+                    </Stack>
+                </AnimatedContainer>
+            </PageContainer>
+        </ScrollArea>
     );
 }
