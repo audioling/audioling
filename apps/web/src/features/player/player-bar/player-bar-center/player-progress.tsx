d9f5e75cba0b23e57fa43b796166db0f1b6c376b
jeffvli <jeffvictorli@gmail.com>
2024-11-27 17:34:42 -0800

    refactor(web): replace custom progress bar component with slider

--- apps/web/src/features/player/player-bar/player-bar-center/player-progress.tsx
+++ apps/web/src/features/player/player-bar/player-bar-center/player-progress.tsx
@@ -1,59 +1,25 @@
-import type { PointerEvent } from 'react';
-import { useRef } from 'react';
-import { motion, useDragControls } from 'motion/react';
+import { Slider } from '@/features/ui/slider/slider.tsx';
 import styles from './player-progress.module.scss';
 
 interface PlayerProgressProps {
     currentTime: number;
     duration: number;
-    onSeek: (time: number) => void;
+    onSeek: (value: number) => void;
 }
 
 export function PlayerProgress(props: PlayerProgressProps) {
-    // const classNames = {
-    //     bar: styles.bar,
-    //     root: styles.root,
-    //     thumb: styles.thumb,
-    // };
-    // return (
-    //     <Slider
-    //         unstyled
-    //         classNames={classNames}
-    //         max={duration}
-    //         min={0}
-    //         value={currentTime}
-    //         w="15rem"
-    //         onChange={onSeek}
-    //     />
-    // );
-
-    const controls = useDragControls();
-
-    const handleClickHandle = (event: PointerEvent<HTMLDivElement>) => {
-        controls.start(event, { snapToCursor: true });
-    };
-
-    const containerRef = useRef<HTMLDivElement>(null);
+    const { currentTime, duration, onSeek } = props;
 
     return (
-        <motion.div
-            {...props}
-            ref={containerRef}
-            className={styles.container}
-            onPointerDown={handleClickHandle}
-        >
-            <motion.div
-                dragDirectionLock
-                className={styles.handle}
-                drag="x"
-                dragConstraints={containerRef}
-                dragControls={controls}
-                dragElastic={0}
-                dragMomentum={false}
-                whileDrag={{ backgroundColor: 'var(--global-primary-color)', scale: 1.5 }}
-                whileHover={{ backgroundColor: 'var(--global-primary-color)', scale: 1.5 }}
+        <div className={styles.container}>
+            <Slider
+                orientation="horizontal"
+                {...props}
+                defaultValue={[currentTime]}
+                max={duration}
+                min={0}
+                onChange={(value) => onSeek(value[0])}
             />
-            <motion.div className={styles.track}></motion.div>
-        </motion.div>
+        </div>
     );
 }
