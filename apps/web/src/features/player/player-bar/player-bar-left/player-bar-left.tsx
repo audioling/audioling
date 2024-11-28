dcbfa1167e19bdae2a7bfde4a5c3ad295a348a36
jeffvli <jeffvictorli@gmail.com>
2024-11-27 18:13:44 -0800

    refactor(web): wire up player bar left

--- apps/web/src/features/player/player-bar/player-bar-left/player-bar-left.tsx
+++ apps/web/src/features/player/player-bar/player-bar-left/player-bar-left.tsx
@@ -1,17 +1,73 @@
+import { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'motion/react';
+import type { TrackItem } from '@/api/api-types.ts';
+import { useAuthBaseUrl } from '@/features/authentication/stores/auth-store.ts';
+import {
+    subscribePlayerQueue,
+    usePlayerActions,
+    usePlayerStore,
+} from '@/features/player/stores/player-store.tsx';
+import { animationVariants } from '@/features/ui/animations/variants.ts';
+import { Image } from '@/features/ui/image/image.tsx';
 import { Text } from '@/features/ui/text/text.tsx';
 import styles from './player-bar-left.module.scss';
 
 export function PlayerBarLeft() {
+    const [currentTrack, setCurrentTrack] = useState<TrackItem | undefined>(undefined);
+    const currentIndex = usePlayerStore.use.player().index;
+    const { getQueue } = usePlayerActions();
+
+    useEffect(() => {
+        const set = () => {
+            const queue = getQueue() || [];
+            setCurrentTrack(queue.items[currentIndex]);
+        };
+
+        const unsub = subscribePlayerQueue(() => {
+            set();
+        });
+
+        set();
+
+        return () => unsub();
+    }, [currentIndex, getQueue]);
+
+    const baseUrl = useAuthBaseUrl();
+
     return (
         <div className={styles.left}>
-            <div className={styles.leftColumn}>
-                <img alt="" className={styles.image} src={''} />
-            </div>
-            <div className={styles.rightColumn}>
-                <Text>Title</Text>
-                <Text isSecondary>Album</Text>
-                <Text isSecondary>Artist</Text>
-            </div>
+            <AnimatePresence mode="wait">
+                <motion.div
+                    key={currentTrack?.id}
+                    animate="show"
+                    className={styles.leftColumn}
+                    exit="hidden"
+                    initial="hidden"
+                    variants={animationVariants.combine(animationVariants.fadeIn)}
+                >
+                    <Image
+                        alt=""
+                        className={styles.image}
+                        src={`${baseUrl}${currentTrack?.imageUrl}&size=100`}
+                    />
+                </motion.div>
+            </AnimatePresence>
+            <motion.div className={styles.rightColumn}>
+                {currentTrack ? (
+                    <>
+                        <Text isEllipsis>{currentTrack.name}</Text>
+                        <Text isEllipsis isSecondary>
+                            {currentTrack.album || 'Unknown Album'}
+                        </Text>
+                        <Text isEllipsis isSecondary>
+                            {currentTrack.artists.map((artist) => artist.name).join(', ') ||
+                                'Unknown Artist'}
+                        </Text>
+                    </>
+                ) : (
+                    <Text>No track selected</Text>
+                )}
+            </motion.div>
         </div>
     );
 }
