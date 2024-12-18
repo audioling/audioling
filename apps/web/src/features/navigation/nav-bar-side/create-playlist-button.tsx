d6ed03dc9651e28dcecadd4782fc7a354e03b9ae
jeffvli <jeffvictorli@gmail.com>
2024-12-18 05:16:27 -0800

    refactor(web): remove menu from create playlist nav

--- apps/web/src/features/navigation/nav-bar-side/create-playlist-button.tsx
+++ apps/web/src/features/navigation/nav-bar-side/create-playlist-button.tsx
@@ -1,9 +1,7 @@
 import { useParams } from 'react-router';
 import { CreatePlaylistModal } from '@/features/playlists/create-playlist/create-playlist-modal.tsx';
-import { CreatePlaylistFolderModal } from '@/features/playlists/create-playlist-folder/create-playlist-folder-modal.tsx';
 import { Group } from '@/features/ui/group/group.tsx';
 import { Icon } from '@/features/ui/icon/icon.tsx';
-import { Menu } from '@/features/ui/menu/menu.tsx';
 import { Text } from '@/features/ui/text/text.tsx';
 import styles from './create-playlist-button.module.scss';
 
@@ -11,23 +9,11 @@ export function CreatePlaylistButton() {
     const { libraryId } = useParams() as { libraryId: string };
 
     return (
-        <Menu align="end" side="bottom">
-            <Menu.Target>
-                <button className={styles.button}>
-                    <Group align="center" gap="md" justify="start" wrap="nowrap">
-                        <Icon icon="add" size="lg" />
-                        <Text size="md">Create New</Text>
-                    </Group>
-                </button>
-            </Menu.Target>
-            <Menu.Content>
-                <Menu.Item onSelect={() => CreatePlaylistModal.call({ libraryId })}>
-                    Create Playlist
-                </Menu.Item>
-                <Menu.Item onSelect={() => CreatePlaylistFolderModal.call({ libraryId })}>
-                    Create Folder
-                </Menu.Item>
-            </Menu.Content>
-        </Menu>
+        <button className={styles.button} onClick={() => CreatePlaylistModal.call({ libraryId })}>
+            <Group align="center" gap="md" justify="start" wrap="nowrap">
+                <Icon icon="add" size="lg" />
+                <Text size="md">Create New</Text>
+            </Group>
+        </button>
     );
 }
