ecf56e12e4b44a4f0ad77d8f46ac1c25a79f1ec9
jeffvli <jeffvictorli@gmail.com>
2024-12-19 18:30:32 -0800

    refactor(web): handle server kill on app close

--- apps/web/src-tauri/src/lib.rs
+++ apps/web/src-tauri/src/lib.rs
@@ -1,24 +1,2 @@
-use tauri_plugin_shell::ShellExt;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .setup(|app| {
-            let sidecar_command = app.shell().sidecar("audioling-server").unwrap();
-            let ( _rx, _child) = sidecar_command
-              .spawn()
-              .expect("Failed to spawn audioling-server");
-
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
--- apps/web/src-tauri/src/main.rs
+++ apps/web/src-tauri/src/main.rs
@@ -1,6 +1,45 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri_plugin_shell::ShellExt;
 
 fn main() {
-    audioling_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![])
+        .setup(|app| {
+            let sidecar_command = app.shell().sidecar("audioling-server").unwrap();
+            let (_rx, _child) = sidecar_command
+                .env("NODE_ENV", "production")
+                .spawn()
+                .expect("Failed to spawn audioling-server");
+
+            Ok(())
+        })
+        .on_window_event(|_event_window, event| {
+            if let tauri::WindowEvent::Destroyed = event {
+                #[cfg(target_os = "windows")]
+                {
+                    use std::process::Command;
+                    use std::os::windows::process::CommandExt;
+
+                    const CREATE_NO_WINDOW: u32 = 0x08000000;
+                    Command::new("taskkill")
+                        .args(["/F", "/IM", "audioling-server.exe"])
+                        .creation_flags(CREATE_NO_WINDOW)
+                        .output()
+                        .expect("Failed to kill audioling-server process");
+                }
+
+                #[cfg(not(target_os = "windows"))]
+                {
+                    use std::process::Command;
+                    Command::new("pkill")
+                        .arg("audioling-server")
+                        .output()
+                        .expect("Failed to kill audioling-server process");
+                }
+            }
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
