fddb316053f322aa4a6fa97551c6979bac5aae2d
jeffvli <jeffvictorli@gmail.com>
2024-12-25 02:23:25 -0800

    refactor(web): memoize image and rowIndex table columns

--- apps/web/src/features/ui/item-list/item-table/columns/image-column.tsx
+++ apps/web/src/features/ui/item-list/item-table/columns/image-column.tsx
@@ -1,31 +1,43 @@
-import type { ColumnHelper } from '@tanstack/react-table';
+import { memo } from 'react';
+import { type ColumnHelper, type Row } from '@tanstack/react-table';
+import type { PlayQueueItem } from '@/api/api-types.ts';
 import { Image } from '@/features/ui/image/image.tsx';
 import { itemListHelpers } from '@/features/ui/item-list/helpers.ts';
-import { Skeleton } from '@/features/ui/skeleton/skeleton.tsx';
 import styles from './column.module.scss';
 
 export function imageColumn<T>(columnHelper: ColumnHelper<T>) {
     return columnHelper.display({
-        cell: ({ row, context }) => {
-            const item = row.original;
-
-            if (typeof item === 'object' && item) {
-                if ('imageUrl' in item && typeof item.imageUrl === 'string') {
-                    return (
-                        <div className={styles.cell}>
-                            <Image
-                                className={styles.image}
-                                src={`${context.baseUrl}${item.imageUrl}&size=100`}
-                            />
-                        </div>
-                    );
-                }
-            }
-
-            return <Skeleton height={20} width={100} />;
-        },
+        cell: MemoizedCell,
         header: '',
         id: 'image',
         size: itemListHelpers.table.numberToColumnSize(60, 'px'),
     });
 }
+
+function Cell<T>({
+    row,
+    context,
+}: {
+    context: { baseUrl: string; currentTrack: PlayQueueItem | undefined };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    row: Row<T | any>;
+}) {
+    const item = row.original;
+
+    if (typeof item === 'object' && item) {
+        if ('imageUrl' in item && typeof item.imageUrl === 'string') {
+            return (
+                <div className={styles.cell}>
+                    <Image
+                        className={styles.image}
+                        src={`${context.baseUrl}${item.imageUrl}&size=100`}
+                    />
+                </div>
+            );
+        }
+    }
+
+    return null;
+}
+
+export const MemoizedCell = memo(Cell);
--- apps/web/src/features/ui/item-list/item-table/columns/row-index-column.tsx
+++ apps/web/src/features/ui/item-list/item-table/columns/row-index-column.tsx
@@ -1,5 +1,8 @@
-import type { ColumnHelper } from '@tanstack/react-table';
+import { memo } from 'react';
+import type { ColumnHelper, Row } from '@tanstack/react-table';
 import clsx from 'clsx';
+import type { PlayQueueItem } from '@/api/api-types.ts';
+import { PlayerStatus, usePlayerStore } from '@/features/player/stores/player-store.tsx';
 import { SoundBars } from '@/features/ui/icon/sound-bars.tsx';
 import { itemListHelpers } from '@/features/ui/item-list/helpers.ts';
 import { Text } from '@/features/ui/text/text.tsx';
@@ -7,30 +10,42 @@ import styles from './column.module.scss';
 
 export function rowIndexColumn<T>(columnHelper: ColumnHelper<T>) {
     return columnHelper.display({
-        cell: ({ row, context }) => {
-            const isPlaying = row.id === context?.currentTrack?._uniqueId;
-            const bpm = context?.currentTrack?.bpm || undefined;
-
-            return (
-                <Text
-                    isCentered
-                    isSecondary
-                    className={clsx(styles.cell, {
-                        [styles.playing]: isPlaying,
-                    })}
-                >
-                    {isPlaying ? (
-                        <>
-                            <SoundBars bpm={bpm} />
-                        </>
-                    ) : (
-                        row.index + 1
-                    )}
-                </Text>
-            );
-        },
+        cell: MemoizedCell,
         header: '#',
         id: 'rowIndex',
         size: itemListHelpers.table.numberToColumnSize(60, 'px'),
     });
 }
+
+function Cell<T>({
+    row,
+    context,
+}: {
+    context: { currentTrack: PlayQueueItem | undefined };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    row: Row<T | any>;
+}) {
+    const isPlaying = row.id === context?.currentTrack?._uniqueId;
+    const bpm = context?.currentTrack?.bpm || undefined;
+    const status = usePlayerStore.use.player().status;
+
+    return (
+        <Text
+            isCentered
+            isSecondary
+            className={clsx(styles.cell, {
+                [styles.playing]: isPlaying,
+            })}
+        >
+            {isPlaying ? (
+                <>
+                    <SoundBars bpm={bpm} isPlaying={status === PlayerStatus.PLAYING} />
+                </>
+            ) : (
+                row.index + 1
+            )}
+        </Text>
+    );
+}
+
+const MemoizedCell = memo(Cell);
