a2f388a70261892ca1d117c14b0b2de0e11c0e8b
jeffvli <jeffvictorli@gmail.com>
2024-12-28 00:20:10 -0800

    refactor(server): remove sharp dependency

--- apps/server/src/modules/image/index.ts
+++ apps/server/src/modules/image/index.ts
@@ -1,27 +1,25 @@
 import { Buffer } from 'buffer';
 import fs from 'fs/promises';
 import path from 'path';
-import sharp from 'sharp';
-import { rgbaToThumbHash } from 'thumbhash';
 import { CONSTANTS } from '@/constants.js';
 
 export const initImageModule = () => {
     return {
-        convertToAvif: async (buffer: Buffer) => {
-            return sharp(buffer).avif().toBuffer();
-        },
-        generateThumbHash: async (imageBuffer: ArrayBuffer) => {
-            const image = sharp(imageBuffer).resize(100, 100, { fit: 'inside' });
-            const { data, info } = await image
-                .ensureAlpha()
-                .raw()
-                .toBuffer({ resolveWithObject: true });
+        // convertToAvif: async (buffer: Buffer) => {
+        //     return sharp(buffer).avif().toBuffer();
+        // },
+        // generateThumbHash: async (imageBuffer: ArrayBuffer) => {
+        //     const image = sharp(imageBuffer).resize(100, 100, { fit: 'inside' });
+        //     const { data, info } = await image
+        //         .ensureAlpha()
+        //         .raw()
+        //         .toBuffer({ resolveWithObject: true });
 
-            const binaryThumbHash = rgbaToThumbHash(info.width, info.height, data);
-            const base64ThumbHash = Buffer.from(binaryThumbHash).toString('base64');
+        //     const binaryThumbHash = rgbaToThumbHash(info.width, info.height, data);
+        //     const base64ThumbHash = Buffer.from(binaryThumbHash).toString('base64');
 
-            return base64ThumbHash;
-        },
+        //     return base64ThumbHash;
+        // },
         getBufferFromPath: async (path: string) => {
             const imageBuffer = await fs.readFile(path);
             return {
@@ -40,9 +38,9 @@ export const initImageModule = () => {
         getCacheLocation: (id: string, libraryId: string, mimetype: string) => {
             return path.join(CONSTANTS.IMAGE_DIR(libraryId), `${id}.${mimetype}`);
         },
-        resize: async (buffer: Buffer, size: { height: number | null; width: number | null }) => {
-            return sharp(buffer).resize(size.width, size.height).toBuffer();
-        },
+        // resize: async (buffer: Buffer, size: { height: number | null; width: number | null }) => {
+        //     return sharp(buffer).resize(size.width, size.height).toBuffer();
+        // },
         writeBufferToFile: async (buffer: Buffer, path: string) => {
             await fs.writeFile(path, buffer);
         },
