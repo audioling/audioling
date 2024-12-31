2d01765d55d9e2f0c2a025b0ab7bcc860900b45d
jeffvli <jeffvictorli@gmail.com>
2024-12-31 00:33:11 -0800

    refactor(web): handle both string and array image src

--- apps/web/src/features/shared/item-image/item-image.tsx
+++ apps/web/src/features/shared/item-image/item-image.tsx
@@ -1,10 +1,14 @@
 import { useAuthBaseUrl } from '@/features/authentication/stores/auth-store.ts';
 import { Image } from '@/features/ui/image/image.tsx';
 
-export function ItemImage(props: { className?: string; size: 'table' | 'card'; src: string[] }) {
+export function ItemImage(props: {
+    className?: string;
+    size: 'table' | 'card';
+    src: string[] | string;
+}) {
     const baseUrl = useAuthBaseUrl();
 
-    const imageUrl = (props.src || []).map(
+    const imageUrl = (Array.isArray(props.src) ? props.src : [props.src]).map(
         (url) => `${baseUrl}${url}&size=${props.size === 'card' ? 300 : 100}`,
     );
 
