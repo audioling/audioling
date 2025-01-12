15280124dcaf1c7e18a5c5c6d414e81d254da5ac
jeffvli <jeffvictorli@gmail.com>
2025-01-12 15:56:22 -0800

    refactor(web): add missing props from SelectInput

--- apps/web/src/features/ui/select-input/select-input.tsx
+++ apps/web/src/features/ui/select-input/select-input.tsx
@@ -17,20 +17,34 @@ interface SelectInputProps {
     label?: string;
     leftSection?: React.ReactNode;
     onChange: (value: string | null) => void;
+    onOpenChange?: (open: boolean) => void;
     radius?: Sizes;
     required?: boolean;
     rightSection?: React.ReactNode;
     size?: Sizes;
+    stickyContent?: React.ReactNode;
     value: string;
 }
 
 export const SelectInput = forwardRef((props: SelectInputProps, ref: Ref<HTMLInputElement>) => {
-    const { data, description, label, onChange, radius, required, size, value } = props;
+    const {
+        data,
+        description,
+        label,
+        onChange,
+        onOpenChange,
+        radius,
+        required,
+        rightSection,
+        size,
+        stickyContent,
+        value,
+    } = props;
 
     const inputValue = data.find((item) => item.value === value)?.label;
 
     return (
-        <Select value={value} onChange={onChange}>
+        <Select value={value} onChange={onChange} onOpenChange={onOpenChange}>
             <Select.Target>
                 <TextInput
                     ref={ref}
@@ -40,17 +54,19 @@ export const SelectInput = forwardRef((props: SelectInputProps, ref: Ref<HTMLInp
                     required={required}
                     rightIcon="arrowDownS"
                     rightIconProps={{ state: 'secondary' }}
+                    rightSection={rightSection}
                     size={size}
                     value={inputValue}
                 />
             </Select.Target>
             <Select.Portal>
-                <Select.Content>
+                <Select.Content stickyContent={stickyContent}>
                     <Select.Viewport>
                         {data.map((item) => (
                             <Select.Item
                                 key={item.value}
                                 disabled={item.disabled}
+                                isSelected={item.value === value}
                                 value={item.value}
                             >
                                 {item.label}
