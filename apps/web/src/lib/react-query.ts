0dcccf8a712f396500d6799de626924633787681
jeffvli <jeffvictorli@gmail.com>
2025-01-15 01:48:04 -0800

    refactor(web): prevent query retry on HTTP 400

--- apps/web/src/lib/react-query.ts
+++ apps/web/src/lib/react-query.ts
@@ -1,10 +1,21 @@
 import { QueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
 export const queryClient = new QueryClient({
     defaultOptions: {
         mutations: {},
         queries: {
-            staleTime: 1000 * 20, // 20 seconds
+            // 20 seconds
+            retry: (_failureCount, error) => {
+                if (error instanceof AxiosError && error.status !== undefined) {
+                    if (error.status >= 400 && error.status < 500) {
+                        return false;
+                    }
+                }
+
+                return true;
+            },
+            staleTime: 1000 * 20,
         },
     },
 });
