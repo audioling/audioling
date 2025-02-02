512b496b057c5a6ba91fa39a4a1cdc99412fe053
jeffvli <jeffvictorli@gmail.com>
2025-02-02 13:32:46 -0800

    refactor(web): set default query gc time to 1 minute

--- apps/web/src/lib/react-query.ts
+++ apps/web/src/lib/react-query.ts
@@ -5,6 +5,7 @@ export const queryClient = new QueryClient({
     defaultOptions: {
         mutations: {},
         queries: {
+            gcTime: 1000 * 60 * 1,
             retry: (_failureCount, error) => {
                 if (error instanceof AxiosError && error.status !== undefined) {
                     if (error.status >= 400 && error.status < 500) {
