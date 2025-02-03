9ca730a5c0d8401d9186e2f9449b9f2010e95c1b
jeffvli <jeffvictorli@gmail.com>
2025-02-02 19:34:20 -0800

    refactor(web): add play button to table image

--- apps/web/src/features/ui/item-list/item-table/columns/image-column.tsx
+++ apps/web/src/features/ui/item-list/item-table/columns/image-column.tsx
@@ -1,12 +1,109 @@
+import type { MouseEvent } from 'react';
+import { LibraryItemType } from '@repo/shared-types';
+import type {
+    AlbumArtistItem,
+    AlbumItem,
+    ArtistItem,
+    GenreItem,
+    PlaylistItem,
+    PlayQueueItem,
+    TrackItem,
+} from '@/api/api-types.ts';
+import { PlayerController } from '@/features/controllers/player-controller.tsx';
+import { PlayType } from '@/features/player/stores/player-store.tsx';
 import { ItemImage } from '@/features/shared/item-image/item-image.tsx';
+import { IconButton } from '@/features/ui/icon-button/icon-button.tsx';
 import type { ItemListCellProps, ItemListColumn } from '@/features/ui/item-list/helpers.ts';
 import { numberToColumnSize } from '@/features/ui/item-list/helpers.ts';
 import { Skeleton } from '@/features/ui/skeleton/skeleton.tsx';
 import styles from './column.module.scss';
 
-function Cell({ item }: ItemListCellProps) {
+const playButtonOmittedItemTypes = [LibraryItemType.PLAYLIST_TRACK];
+
+function Cell({ item, itemType, isHovered }: ItemListCellProps) {
     if (typeof item === 'object' && item) {
         if ('imageUrl' in item) {
+            const handlePlay = (e: MouseEvent<HTMLButtonElement>) => {
+                e.stopPropagation();
+
+                switch (itemType) {
+                    case LibraryItemType.ALBUM:
+                        PlayerController.call({
+                            cmd: {
+                                addToQueueByFetch: {
+                                    id: [(item as AlbumItem).id],
+                                    itemType: itemType,
+                                    type: PlayType.NOW,
+                                },
+                            },
+                        });
+                        break;
+                    case LibraryItemType.ALBUM_ARTIST:
+                        PlayerController.call({
+                            cmd: {
+                                addToQueueByFetch: {
+                                    id: [(item as AlbumArtistItem).id],
+                                    itemType: itemType,
+                                    type: PlayType.NOW,
+                                },
+                            },
+                        });
+                        break;
+                    case LibraryItemType.ARTIST:
+                        PlayerController.call({
+                            cmd: {
+                                addToQueueByFetch: {
+                                    id: [(item as ArtistItem).id],
+                                    itemType: itemType,
+                                    type: PlayType.NOW,
+                                },
+                            },
+                        });
+                        break;
+                    case LibraryItemType.GENRE:
+                        PlayerController.call({
+                            cmd: {
+                                addToQueueByFetch: {
+                                    id: [(item as unknown as GenreItem).id],
+                                    itemType: itemType,
+                                    type: PlayType.NOW,
+                                },
+                            },
+                        });
+                        break;
+                    case LibraryItemType.PLAYLIST:
+                        PlayerController.call({
+                            cmd: {
+                                addToQueueByFetch: {
+                                    id: [(item as PlaylistItem).id],
+                                    itemType: itemType,
+                                    type: PlayType.NOW,
+                                },
+                            },
+                        });
+                        break;
+                    case LibraryItemType.PLAYLIST_TRACK:
+                        break;
+                    case LibraryItemType.TRACK:
+                        PlayerController.call({
+                            cmd: {
+                                addToQueueByData: {
+                                    data: [item as TrackItem],
+                                    type: PlayType.NOW,
+                                },
+                            },
+                        });
+                        break;
+                    case LibraryItemType.QUEUE_TRACK:
+                        PlayerController.call({
+                            cmd: {
+                                mediaPlay: { id: (item as PlayQueueItem)._uniqueId },
+                            },
+                        });
+                        break;
+                }
+            };
+
             return (
                 <div className={styles.cell}>
                     <ItemImage
@@ -14,6 +111,16 @@ function Cell({ item }: ItemListCellProps) {
                         size="table"
                         src={item.imageUrl as string | string[]}
                     />
+                    {isHovered && !playButtonOmittedItemTypes.includes(itemType) && (
+                        <IconButton
+                            iconFill
+                            className={styles.playButton}
+                            icon="mediaPlay"
+                            size="xs"
+                            variant="filled"
+                            onClick={handlePlay}
+                        />
+                    )}
                 </div>
             );
         }
